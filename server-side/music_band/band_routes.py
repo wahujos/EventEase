11eaacb96af1137c55509d07aa18a7db1fdea2f1
wahujos <jwahu30@gmail.com>
2024-03-29 16:51:23 +0300

    routes data for mcs and artists

--- server-side/music_band/band_routes.py
+++ server-side/music_band/band_routes.py
@@ -1,5 +1,6 @@
 """importing the required modules"""
 from flask import Blueprint, jsonify
+import requests
 
 # Create an instance of the blueprint
 music_band = Blueprint("music_band", __name__)
@@ -9,7 +10,17 @@ def get_all_bands():
     """
     This function gets all details of the music bands
     """
-    return jsonify({"message": "Gets all details of the music bands"})
+    try:
+        userdata = requests.get("https://dummyjson.com/users")
+        userdata.raise_for_status()  # Raise exception for HTTP errors (4xx, 5xx)
+
+        useful_data = userdata.json()
+        users = useful_data.get('users', [])
+        return jsonify({"users": users})
+    except requests.RequestException as e:
+        # Log the error
+        print("Error fetching catering data:", e)
+        return jsonify({"message": "Failed to fetch catering data"}), 500  # Internal Server Error
 
 @music_band.route("/music_band/<user_id>", strict_slashes=False)
 def get_given_band(user_id):
